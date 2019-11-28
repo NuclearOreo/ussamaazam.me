6d0487235aaf3cbd5dc77254ec5fc2c63006677d
Ussama Azam <nuclearoreo@gmail.com>
2019-11-28 15:12:41 -0500

    Finished LevenshtienDistance

--- src/components/problems/levenshtein.vue
+++ src/components/problems/levenshtein.vue
@@ -67,10 +67,23 @@
             >
               <pre
                 style="margin: 0; line-height: 125%"
-              ><span style="color: #66d9ef">def</span> <span style="color: #a6e22e">kadane</span><span style="color: #f8f8f2">(nums):</span>
-    <span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">i</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #f8f8f2">len(nums)):</span>
-        <span style="color: #f8f8f2">nums[i]</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">max(nums[i],</span> <span style="color: #f8f8f2">nums[i]</span> <span style="color: #f92672">+</span> <span style="color: #f8f8f2">nums[i</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">])</span>
-    <span style="color: #66d9ef">return</span> <span style="color: #ae81ff">0</span> <span style="color: #66d9ef">if</span> <span style="color: #f8f8f2">nums[</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span> <span style="color: #f92672">&lt;</span> <span style="color: #ae81ff">0</span> <span style="color: #66d9ef">else</span> <span style="color: #f8f8f2">nums[</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span> 
+              ><span style="color: #66d9ef">def</span> <span style="color: #a6e22e">levenshteinDistance</span><span style="color: #f8f8f2">(str1,</span> <span style="color: #f8f8f2">str2):</span>
+	<span style="color: #f8f8f2">m</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">[[</span><span style="color: #ae81ff">0</span><span style="color: #f8f8f2">]</span> <span style="color: #f92672">*</span> <span style="color: #f8f8f2">(len(str1)</span><span style="color: #f92672">+</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">)</span> <span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">i</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(len(str2)</span><span style="color: #f92672">+</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">)]</span>
+	
+	<span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">i</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(len(m)):</span>
+		<span style="color: #f8f8f2">m[i][</span><span style="color: #ae81ff">0</span><span style="color: #f8f8f2">]</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">i</span>
+		
+	<span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">i</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(len(m[</span><span style="color: #ae81ff">0</span><span style="color: #f8f8f2">])):</span>
+		<span style="color: #f8f8f2">m[</span><span style="color: #ae81ff">0</span><span style="color: #f8f8f2">][i]</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">i</span>
+		
+	<span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">r</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #f8f8f2">len(m)):</span>
+		<span style="color: #66d9ef">for</span> <span style="color: #f8f8f2">c</span> <span style="color: #f92672">in</span> <span style="color: #f8f8f2">range(</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #f8f8f2">len(m[</span><span style="color: #ae81ff">0</span><span style="color: #f8f8f2">])):</span>
+			<span style="color: #66d9ef">if</span> <span style="color: #f8f8f2">str1[c</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span> <span style="color: #f92672">==</span> <span style="color: #f8f8f2">str2[r</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]:</span>
+				<span style="color: #f8f8f2">m[r][c]</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">m[r</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">][c</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">]</span>
+			<span style="color: #66d9ef">else</span><span style="color: #f8f8f2">:</span>
+				<span style="color: #f8f8f2">m[r][c]</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">min(m[r</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">][c</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">],</span> <span style="color: #f8f8f2">m[r][c</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">],</span> <span style="color: #f8f8f2">m[r</span><span style="color: #f92672">-</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">][c])</span> <span style="color: #f92672">+</span> <span style="color: #ae81ff">1</span>
+	
+	<span style="color: #66d9ef">return</span> <span style="color: #f8f8f2">m[len(str2)][len(str1)]</span>
 </pre>
             </div>
           </div>
@@ -89,20 +102,19 @@ export default {
     return {
       problem:
         "# Levenshtein Distance\n\n" +
-        "The bubble sort is also known as the ripple sort. The bubble sort is probably the first, reasonably complex module that any beginning programmer has to write. It is a very simple construct which introduces the student to the fundamentals of how sorting works.\n\n" +
-        "A bubble sort makes use of an array and some sort of swapping mechanism. Most programming languages have a built-in function to swap elements of an array. Even if a swapping function does not exist, only a couple of extra lines of code are required to store one\n" +
-        "array element in a temporary field in order to swap a second element into its place. Then the first element is moved out of the temporary field and back into the array at the second element's position.\n\n" +
-        "Here is a simple example of how a bubble sort works: Suppose you have a row of children's toy blocks with letters on them. They are in random order and you wish to arrange them in alphabetical order from left to right.\n\n" +
-        "1. Begin with the first block. In this case, the letter G.\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/1/19/Bubble01.png)\n\n" +
-        "2. Look at the block just to the right of it.\n\n" +
-        "3. If the block to the right should come before the block on the left, swap them so that they are in order\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/3/3b/Bubble02.png)\n\n" +
-        "4. Compare the next block in line with the first, and repeat step 3. Do this until you run out of blocks. Then begin step one again with the second block.\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/b/bf/Bubble03.png)\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/2/27/Bubble04.png)\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/7/76/Bubble05.png)\n\n\t" +
-        "![alt text](https://upload.wikimedia.org/wikibooks/en/a/a4/Bubble06.png)"
+        "Mathematically, the Levenshtein distance between two strings a, b (of length |a| and |b| respectively) is given by leva,b(|a|,|b|) where:\n\n" +
+        "![alt text](https://wikimedia.org/api/rest_v1/media/math/render/svg/4520f5376b54613a5b0e6c6db46083989f901821)\n\n" +
+        "where 1(ai≠bi) is the indicator function equal to 0 when ai≠bi and equal to 1 otherwise, and leva, b(i,j) is the distance between the first i characters of a and the first j characters of b. \n\n" +
+        "Note that the first element in the minimum corresponds to deletion (from a to b), the second to insertion and the third to match or mismatch, depending on whether the respective symbols are the same.\n\n" +
+        "The Levenshtein algorithm calculates the least number of edit operations that are necessary to modify one string to obtain another string. The most common way of calculating this is by the dynamic programming approach:\n\n" +
+        "- A matrix is initialized measuring in the (m, n) cell the Levenshtein distance between the m-character prefix of one with the n-prefix of the other word.\n" +
+        "- The matrix can be filled from the upper left to the lower right corner.\n" +
+        "- Each jump horizontally or vertically corresponds to an insert or a delete, respectively\n" +
+        "- The cost is normally set to 1 for each of the operations.\n" +
+        "- The diagonal jump can cost either one, if the two characters in the row and column do not match else 0, if they match. Each cell always minimizes the cost locally.\n" +
+        "- This way the number in the lower right corner is the Levenshtein distance between both words.\n\n" +
+        'An example that features the comparison of "HONDA" and "HYUNDAI",\n\n' +
+        "![alt text](https://23o0161033pm1289qo1hzrwi-wpengine.netdna-ssl.com/wp-content/uploads/2017/01/L1.jpg)"
     };
   },
   methods: {
