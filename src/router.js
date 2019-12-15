6ba4a3fee9f585d036e0e07fa1d1f22a3729811d
Ussama Azam <nuclearoreo@gmail.com>
2019-12-15 18:09:05 -0500

    Need to rethink the lost page

--- src/router.js
+++ src/router.js
@@ -28,6 +28,11 @@ export default new Router({
     },
     {
       path: "*",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "LOST",
       name: "lost",
       component: Lost
     }
