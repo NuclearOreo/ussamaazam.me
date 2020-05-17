490247e3af28949d6115c9edd838d043d0e99ee2
Ussama Azam <nuclearoreo@gmail.com>
2020-05-17 00:56:42 -0400

    Added base to to check on website, need to send a notification now

--- ghetto-healthcheck/script.py
+++ ghetto-healthcheck/script.py
@@ -1,3 +1,25 @@
-import requests
+import pycurl
+from io import BytesIO 
 
-r = requests.get('https://www.ussamaazam.me/', stream=True)
+b_obj = BytesIO() 
+crl = pycurl.Curl() 
+
+# Set URL value
+crl.setopt(crl.URL, 'https://www.ussamaazam.me/')
+
+# Write bytes that are utf-8 encoded
+crl.setopt(crl.WRITEDATA, b_obj)
+
+try:
+    # Perform a file transfer 
+    crl.perform()
+    get_body = b_obj.getvalue()
+
+    # Decode the bytes stored in get_body to HTML and print the result 
+    print('Output of GET request:\n%s' % get_body.decode('utf8')) 
+
+    crl.close()
+except:
+    print("Something went wrong :(")
+
+    crl.close()
