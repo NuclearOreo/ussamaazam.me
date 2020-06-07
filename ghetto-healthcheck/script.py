895c2712daf791d9e5cc99badca0a4d7ecf08bc0
Ussama Azam <nuclearoreo@gmail.com>
2020-06-06 22:50:07 -0400

    Able to send emails

--- ghetto-healthcheck/script.py
+++ ghetto-healthcheck/script.py
@@ -1,11 +1,13 @@
 import pycurl
-from io import BytesIO 
+from io import BytesIO
+import smtplib
+import os
 
 b_obj = BytesIO() 
 crl = pycurl.Curl() 
 
 # Set URL value
-crl.setopt(crl.URL, 'https://www.ussamaazam.me/')
+crl.setopt(crl.URL, 'https://www.ussamazam.me/')
 
 # Write bytes that are utf-8 encoded
 crl.setopt(crl.WRITEDATA, b_obj)
@@ -20,6 +22,20 @@ try:
 
     crl.close()
 except:
-    print("Something went wrong :(")
+    sender = os.getenv('SENDER')
+    receiver = [os.getenv('RECIEVER')]
+    password = os.getenv('EMAIL_PASSWORD')
+
+    message = """
+                Your website is down.
+              """
+
+
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender, password)
+
+    server.sendmail(sender, receiver, message)
 
     crl.close()
