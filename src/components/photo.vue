ca18241912832dee12c263ad4ed9160a5acb4d65
Ussama Azam <nuclearoreo@gmail.com>
2020-11-26 16:50:43 -0500

    Removing box shadows

--- src/components/photo.vue
+++ src/components/photo.vue
@@ -19,7 +19,6 @@ export default {
   vertical-align: middle;
   -webkit-transform: perspective(1px) translateZ(0);
   transform: perspective(1px) translateZ(0);
-  box-shadow: 0 0 0 rgba(0, 0, 0, 0);
   -webkit-transition-duration: 0.3s;
   transition-duration: 0.3s;
   -webkit-transition-property: transform;
