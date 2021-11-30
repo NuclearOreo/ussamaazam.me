becc11a30696d4970c827bb18bdcff8602e0d151
Ussama Azam <nuclearoreo@gmail.com>
2021-11-30 13:28:29 -0500

    Adding Colors

---
+++ src/Styles/color.ts
@@ -0,0 +1,39 @@
+export const primary = {
+  light1: '#9de5e2',
+  light2: '#adf6f3',
+  neutral: '#85cdca',
+  dark2: '#71b8b6',
+  dark1: '#5ba29f',
+}
+
+export const secondary = {
+  light1: '#ffb392',
+  light2: '#ff9778',
+  neutral: '#e27d5f',
+  dark2: '#d16e52',
+  dark1: '#c05f44',
+}
+
+export const otherColor1 = {
+  light1: '#ffd0a2',
+  light2: '#fcb98c',
+  neutral: '#e8a87c',
+  dark2: '#d6966b',
+  dark1: '#c5875c',
+}
+
+export const otherColor2 = {
+  light1: '#f8becf',
+  light2: '#dca5b5',
+  neutral: '#c38d9d',
+  dark2: '#a57080',
+  dark1: '#946171',
+}
+
+export const otherColor3 = {
+  light1: '#7ae9d6',
+  light2: '#60d1be',
+  neutral: '#40b3a2',
+  dark2: '#209d8c',
+  dark1: '#008979',
+}
