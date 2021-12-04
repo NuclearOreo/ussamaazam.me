877aff92d7871bebc50df528dec29ff6e8eb8f3d
Ussama Azam <nuclearoreo@gmail.com>
2021-12-04 13:24:40 -0500

    Added Color

--- src/Components/Icon/IconStyle.ts
+++ src/Components/Icon/IconStyle.ts
@@ -1,8 +1,10 @@
 import { css } from '@emotion/css'
+import { GunMetal } from '../../Styles/color'
 
 export const IconStyles = css`
   font-size: 2px;
   letter-spacing: -0.45px;
+  color: ${GunMetal.dark3};
 `
 
 export const containerStyle = css`
