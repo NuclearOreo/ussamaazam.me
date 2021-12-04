1e74eedd736cb8a2825bad85dac7035888e80e5e
Ussama Azam <nuclearoreo@gmail.com>
2021-12-04 12:51:25 -0500

    Adding Icon

---
+++ src/Components/Icon/IconStyle.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/css'
+
+export const IconStyles = css`
+  font-size: 2px;
+  letter-spacing: -0.45px;
+`
+
+export const containerStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
