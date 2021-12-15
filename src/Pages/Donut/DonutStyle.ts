59c9551bf627198cb052cee23bda9fbec68ba6a1
Ussama Azam <nuclearoreo@gmail.com>
2021-12-14 22:20:31 -0500

    Making the donut responsive

--- src/Pages/Donut/DonutStyle.ts
+++ src/Pages/Donut/DonutStyle.ts
@@ -6,5 +6,11 @@ export const style = css`
   align-items: center;
   justify-content: center;
   font-size: 1.5vh;
+  transform: translate(0, 10%);
   color: ${GunMetal.original};
+
+  @media screen and (max-aspect-ratio: 3/2) {
+    font-size: 1vh;
+    transform: translate(0, 40%);
+  }
 `
