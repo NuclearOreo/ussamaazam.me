2a1ff036243dbc672ae8ef776b59bb489aac4f5d
Ussama Azam <nuclearoreo@gmail.com>
2022-01-31 20:27:19 +0000

    replacing word

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -1,7 +1,7 @@
 import { style } from './BlogStyle'
 
 const Blog = () => {
-  return <div className={style}>This is Essays</div>
+  return <div className={style}>This is Blogs</div>
 }
 
 export default Blog
