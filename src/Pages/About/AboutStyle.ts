56b9eafb60beece0e26afe7eb5ed3b8999ff67b9
Ussama Azam <nuclearoreo@gmail.com>
2022-02-23 04:08:36 +0000

    Added my text

--- src/Pages/About/AboutStyle.ts
+++ src/Pages/About/AboutStyle.ts
@@ -1,14 +1,16 @@
 import { css } from '@emotion/css'
 import { GunMetal, DesertSand } from 'Styles/color'
-import { fontFamily } from 'Styles/fonts'
+import { fontFamily, } from 'Styles/fonts'
 
-export const style = css`
-  color: ${GunMetal.original}};
-  // font-family: ${fontFamily.Poppins}
-
+export const style = css`  
   .center {
+    color: ${GunMetal.original};
+    font-family: ${fontFamily.Poppins};
+    font-size: 18px;
     margin: auto;
     width: 60%;
+    letter-spacing: 0.3px;
+    text-align: center;
     border: 3px solid ${DesertSand.original};
   }
 `
--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -3,7 +3,17 @@ import { style } from './AboutStyle'
 const About = () => {
   return (
     <div className={style}>
-      <div className="center">This is the about Page</div>
+      <div className="center">
+        Hey, my name is Ussama Azam, and I'm a young software engineer early in my career. I have
+        many interests that range from photography to random technical stuff to more random
+        technical stuff. It seems to me that I'm a lifelong learner cause I can't seem to stop
+        reading Hacker News and trying random projects, an obsession fueled by boredom. Ever since I
+        learned about Y Combinator and constantly reading the great blogs that always pop up on the
+        site, I've decided to attempt writing blogs. The whole goal of this website is to motivate
+        me to start writing blogs (hopefully become a better writer in the process) and showcase my
+        photos from my photography venture. Maybe I'll decide to go out of my way to showcase the
+        exciting stuff I made, like the stupid donut on my home page.{' '}
+      </div>
     </div>
   )
 }
