b0d23932ff87e206cd561ab17ebd64f20011a978
Ussama Azam <nuclearoreo@gmail.com>
2022-03-13 18:38:22 +0000

    Stupid commit

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -1,7 +1,7 @@
 import { style } from './BlogStyle'
 
 function Blog() {
-  return <div className={style}>This is Blogs?</div>
+  return <div className={style}>This is Blogs</div>
 }
 
 export default Blog
