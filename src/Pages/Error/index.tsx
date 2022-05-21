bd10a269b829f6c9c24967fcdd66c978cc1ee2da
Ussama Azam <nuclearoreo@gmail.com>
2022-05-21 23:23:48 +0000

    Particle effects

--- src/Pages/Error/index.tsx
+++ src/Pages/Error/index.tsx
@@ -1,5 +1,130 @@
-import { style } from './styles'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import { Engine } from 'tsparticles-engine'
 
 export function ErrorPage() {
-  return <div className={style}>This is the Error Page</div>
+  const particlesInit = async (main: Engine) => {
+    await loadFull(main)
+  }
+
+  const particlesLoaded = async (container: unknown) => {
+    // eslint-disable-next-line no-console
+    console.log(container)
+  }
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: 1,
+        },
+        particles: {
+          number: {
+            value: 160,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: '#ffffff',
+          },
+          shape: {
+            type: 'circle',
+          },
+          opacity: {
+            value: 1,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 4,
+              size_min: 0.3,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+            distance: 150,
+            color: '#ffffff',
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: 'none',
+            random: true,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 600,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'bubble',
+            },
+            onclick: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 250,
+              size: 0,
+              duration: 2,
+              opacity: 0,
+            },
+            repulse: {
+              distance: 400,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: true,
+        background: {
+          color: '#232741',
+          image:
+            "url('http://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1237px-NASA_logo.svg.png')",
+          position: '50% 50%',
+          repeat: 'no-repeat',
+          size: '20%',
+        },
+      }}
+    />
+  )
 }
