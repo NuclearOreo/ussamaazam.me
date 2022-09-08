31fa70eef7aba27c3d011e3be0c838cc4d3e352f
Ussama Azam <nuclearoreo@gmail.com>
2022-09-08 16:12:14 +0000

    Using clamp for fontsize

--- src/Components/Donut3D/styles.ts
+++ src/Components/Donut3D/styles.ts
@@ -5,9 +5,9 @@ export const canvasStyle = css`
   width: 30vw;
   height: 30vw;
 
-  @media (min-width: 481px) and (max-width: 767px) {
-    width: 50vw;
-    height: 50vw;
+  @media (max-width: 767px) {
+    width: 100vw;
+    height: 100vw;
   }
 
   @media (min-width: 768px) and (max-width: 1024px) {
