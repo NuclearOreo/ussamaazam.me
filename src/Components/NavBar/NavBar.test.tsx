9aa5afe2f0617df7b5f76013d5be6106e5e8817e
Ussama Azam <nuclearoreo@gmail.com>
2022-10-24 18:24:18 +0000

    Fixed Unit test for navBar

--- src/Components/NavBar/NavBar.test.tsx
+++ src/Components/NavBar/NavBar.test.tsx
@@ -1,17 +1,19 @@
-import { render /* screen */ } from '@testing-library/react'
-// import NavBar from '.'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import { NavBar } from '.'
 
 test('Render NavBar Component', () => {
-  render(<>Hello</>)
-  //   // const HomeLink = screen.getByText(/🍩/i)
-  //   // const AboutLink = screen.getByText(/About/i)
-  //   // const GalleryLink = screen.getByText(/Gallery/i)
-  //   // const EssaysLink = screen.getByText(/Essays/i)
-
-  //   // expect(HomeLink).toBeInTheDocument()
-  //   // expect(AboutLink).toBeInTheDocument()
-  //   // expect(GalleryLink).toBeInTheDocument()
-  //   // expect(EssaysLink).toBeInTheDocument()
-  //   // expect(HomeLink).toBeInTheDocument()
-  expect(2 + 2).toEqual(4)
+  render(
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+      </Routes>
+    </Router>,
+  )
+  const HomeLink = screen.getByText(/🍩/i)
+  const AboutLink = screen.getByText(/About/i)
+  const GalleryLink = screen.getByText(/Gallery/i)
+  expect(HomeLink).toBeInTheDocument()
+  expect(AboutLink).toBeInTheDocument()
+  expect(GalleryLink).toBeInTheDocument()
 })
