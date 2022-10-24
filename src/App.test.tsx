7aa05821d2df7f3f813396b1a486cc14f20dda04
Ussama Azam <nuclearoreo@gmail.com>
2022-10-24 20:22:42 +0000

    Adding small test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render /* screen */ } from '@testing-library/react'
-// import App from './App'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<>Hello</>)
+  render(<App />)
   // const linkElement = screen.getByTestId('Donut')
   // expect(linkElement).toBeInTheDocument()
   expect(5).toEqual(5)
