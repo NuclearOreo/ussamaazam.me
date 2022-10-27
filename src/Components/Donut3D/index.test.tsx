fb4ba69b21d6fcb46f0ebe9cb48650db72def516
Ussama Azam <nuclearoreo@gmail.com>
2022-10-27 18:33:30 +0000

    Threshold

--- src/Components/Donut3D/index.test.tsx
+++ src/Components/Donut3D/index.test.tsx
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react'
 import { Donut3D } from '.'
+import Donut48 from './Donut48'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { ResizeObserver } from '@juggle/resize-observer'
 
-test('Render 3D Donut', () => {
-  render(<Donut3D />)
-  const canvas = screen.getByTestId('Canvas')
-  expect(canvas).toBeInTheDocument()
+describe('Tests for 3D donut', () => {
+  test('Render 3D Donut', () => {
+    render(<Donut3D />)
+    const canvas = screen.getByTestId('Canvas')
+    expect(canvas).toBeInTheDocument()
+  })
+  test('Render Donut48', () => {
+    render(
+      <Canvas resize={{ polyfill: ResizeObserver }}>
+        <ambientLight intensity={1} />
+        <Suspense fallback={null}>
+          <Donut48 scale={27} />
+        </Suspense>
+      </Canvas>,
+    )
+    expect(1 + 1).toEqual(2)
+  })
 })
