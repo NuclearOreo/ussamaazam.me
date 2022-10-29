15a2a7f5f679ad5794df9d5f408dd0d3ced8112f
Ussama Azam <nuclearoreo@gmail.com>
2022-10-29 00:11:50 +0000

    Testing Loading

--- src/APIs/Unsplash/index.test.tsx
+++ src/APIs/Unsplash/index.test.tsx
@@ -1,16 +1,42 @@
 import { act, renderHook } from '@testing-library/react-hooks'
+import { AxiosResponse } from 'axios'
+import { UnsplashType } from './types'
 import { photoPagination, unsplash } from '.'
 
 jest.mock('./Unsplash')
+
+const getControlledPromise = () => {
+  let deferred
+
+  const promise = new Promise<AxiosResponse<UnsplashType[]>>((resolve, reject) => {
+    deferred = { resolve, reject }
+  })
+
+  return { deferred, promise }
+}
 
 describe('Testing Photo Pagination hook', () => {
   test('Able to photos', async () => {
     await act(async () => {
       renderHook(() => {
-        photoPagination()
+        photoPagination(0, 0)
       })
     })
 
-    expect(unsplash.getMyPhoto).toBeCalledTimes(1)
+    expect(unsplash.getMyPhoto).toBeCalledWith({ pageNumber: 0, perPage: 0 })
+  })
+  test('Testing loading', async () => {
+    const { deferred, promise } = getControlledPromise()
+
+    unsplash.getMyPhoto = jest.fn(() => promise)
+
+    const { result, waitForNextUpdate } = renderHook(photoPagination)
+
+    expect(result.current.loading).toEqual(true)
+
+    deferred.resolve()
+    await waitForNextUpdate()
+
+    expect(result.current.loading).toEqual(false)
   })
 })
