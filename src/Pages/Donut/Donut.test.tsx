bd26b6786dfd5e4678a1515ca55824bd2a86cdec
Ussama Azam <nuclearoreo@gmail.com>
2022-10-29 20:29:52 +0000

    Testing interval

--- src/Pages/Donut/Donut.test.tsx
+++ src/Pages/Donut/Donut.test.tsx
@@ -3,19 +3,44 @@ import React from 'react'
 import { act } from 'react-dom/test-utils'
 import { DonutPage } from '.'
 
-test('Render Home Component', () => {
-  const disableMobile = true
-  render(<DonutPage disableMobile={disableMobile} />)
-  const donut = screen.getByTestId('Donut')
-  const wrapper = screen.getByTestId('Wrapper')
+const doAsync = (c: any) => {
+  setTimeout(() => {
+    c(true)
+  }, 3000)
+}
 
-  expect(donut).toBeInTheDocument()
-  expect(wrapper).toBeInTheDocument()
+jest.useFakeTimers()
 
-  act(() => {
-    wrapper.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+describe('Home Page', () => {
+  test('Render Home Page', () => {
+    const disableMobile = true
+    render(<DonutPage disableMobile={disableMobile} />)
+    const donut = screen.getByTestId('Donut')
+    const wrapper = screen.getByTestId('Wrapper')
+
+    expect(donut).toBeInTheDocument()
+    expect(wrapper).toBeInTheDocument()
+
+    act(() => {
+      wrapper.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    const canvas = screen.getByTestId('Canvas')
+    expect(canvas).toBeInTheDocument()
   })
+  test('Test Set interval', () => {
+    const disableMobile = true
+    render(<DonutPage disableMobile={disableMobile} />)
 
-  const canvas = screen.getByTestId('Canvas')
-  expect(canvas).toBeInTheDocument()
+    act(() => {
+      jest.advanceTimersByTime(50)
+
+      const callback1 = () => {
+        expect(screen.getByTestId('Donut')).toBeInTheDocument()
+      }
+
+      doAsync(callback1)
+      jest.useRealTimers()
+    })
+  })
 })
