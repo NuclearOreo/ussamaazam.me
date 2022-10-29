ed9648c6e1e96b5d0d69cc8522cd0bdb24b08161
Ussama Azam <nuclearoreo@gmail.com>
2022-10-29 19:05:33 +0000

    Unit test for unsplash

---
+++ src/APIs/Unsplash/Unsplash.test.ts
@@ -0,0 +1,15 @@
+import Unsplash from './Unsplash'
+import axios from 'axios'
+
+jest.mock('axios')
+
+describe('Test for Unsplash Endpoints', () => {
+  test('Testing Building Object', () => {
+    axios.create = jest.fn()
+    new Unsplash()
+    expect(axios.create).toBeCalledWith({ baseURL: 'https://unsplash-api-dsoicsjfka-uc.a.run.app' })
+  })
+  test('Testing getMyPhoto', () => {
+    expect(1).toEqual(1)
+  })
+})
