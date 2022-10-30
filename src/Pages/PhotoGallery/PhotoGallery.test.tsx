7760cfa2c0a03d286cc0893532c62577f8a22dde
Ussama Azam <nuclearoreo@gmail.com>
2022-10-30 20:50:46 +0000

    Reload test

--- src/Pages/PhotoGallery/PhotoGallery.test.tsx
+++ src/Pages/PhotoGallery/PhotoGallery.test.tsx
@@ -1,11 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import React from 'react'
 import { PhotoGalleryPage } from '.'
 
-test('Render PhotoGallery Component', () => {
-  render(<PhotoGalleryPage />)
-  const linkElement = screen.getByText(
-    /I'm a hobbyist photographer outside of work and pushing myself to come up with with creative work. Hope you enjoy my photos us much as I do!/i,
-  )
-  expect(linkElement).toEqual(linkElement)
+describe('Render PhotoGallery Component', () => {
+  test('Check if my description is present', () => {
+    render(<PhotoGalleryPage />)
+    const description = screen.getByText(
+      /I'm a hobbyist photographer outside of work and pushing myself to come up with with creative work. Hope you enjoy my photos us much as I do!/i,
+    )
+    expect(description).toEqual(description)
+  })
+  test('Resize window research', () => {
+    render(<PhotoGalleryPage />)
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: jest.fn() },
+    })
+
+    act(() => {
+      global.innerWidth = 500
+      global.dispatchEvent(new Event('resize'))
+    })
+
+    expect(window.location.reload).toBeCalled()
+  })
 })
